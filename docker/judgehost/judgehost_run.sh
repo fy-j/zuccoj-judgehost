908b7c8fc8c5176169cf92e1c5f930592d815e81
fy-j <1289757736@qq.com>
2022-10-15 20:47:22 +0800

    日志展示

--- docker/judgehost/judgehost_run.sh
+++ docker/judgehost/judgehost_run.sh
@@ -1,2 +1,3 @@
 #!  /bin/bash
-java -jar /root/judgehost/zuccoj-judgehost.jar >/root/judgehost/judgehost_log.log 2>/root/judgehost/judgehost_err.log
+#java -jar /root/judgehost/zuccoj-judgehost.jar >/root/judgehost/judgehost_log.log 2>/root/judgehost/judgehost_err.log
+java -jar /root/judgehost/zuccoj-judgehost.jar
