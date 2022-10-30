f9ef0634fa8072ca4310c8139b24575f9be8fa39
fy-j <1289757736@qq.com>
2022-10-30 19:46:53 +0800

    sh修改

--- docker/judgehost/judgehost_run.sh
+++ docker/judgehost/judgehost_run.sh
@@ -1,3 +1,3 @@
 #!  /bin/bash
-#java -jar /root/judgehost/zuccoj-judgehost.jar >/root/judgehost/judgehost_log.log 2>/root/judgehost/judgehost_err.log
-java -jar /root/judgehost/zuccoj-judgehost.jar
+java -jar /root/judgehost/zuccoj-judgehost.jar >/root/judgehost/judgehost_log.log 2>/root/judgehost/judgehost_err.log
+#java -jar /root/judgehost/zuccoj-judgehost.jar
